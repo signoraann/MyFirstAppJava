7eed96db3c461dae8c2185b5f0ccfeee7e830ceb
signoraann <atumanova796@gmail.com>
2026-07-22 15:35:02 +0300

    Used different logs levels

--- src/main/java/com/signoraann/javalearning/lesson21/Main.java
+++ src/main/java/com/signoraann/javalearning/lesson21/Main.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        logger.info("Message");
+        logger.debug("sum = 10");
+        logger.info("Item added to cart");
+        logger.warn("Low disk space");
+        logger.error("Database connection error");
     }
 }
