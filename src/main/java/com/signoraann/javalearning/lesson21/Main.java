56734d80f0a719633c0484bc0b2f856ba2bffc99
signoraann <atumanova796@gmail.com>
2026-07-22 15:57:59 +0300

    Used  the "placeholder" syntax

--- src/main/java/com/signoraann/javalearning/lesson21/Main.java
+++ src/main/java/com/signoraann/javalearning/lesson21/Main.java
@@ -3,12 +3,12 @@ package com.signoraann.javalearning.lesson21;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        String username = "Alice";
         logger.debug("sum = 10");
-        logger.info("Item added to cart");
+        logger.info("User {} logged in", username);
         logger.warn("Low disk space");
         logger.error("Database connection error");
     }
