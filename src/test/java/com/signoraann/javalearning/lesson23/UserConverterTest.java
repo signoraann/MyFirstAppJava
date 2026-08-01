6be7ba4a2d42655bd9f12e4b14e5b66f845903fa
signoraann <atumanova796@gmail.com>
2026-08-01 17:51:15 +0300

    Removed public modifier from test classes

--- src/test/java/com/signoraann/javalearning/lesson23/UserConverterTest.java
+++ src/test/java/com/signoraann/javalearning/lesson23/UserConverterTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class UserConverterTest {
+class UserConverterTest {
     private final UserConverter converter = new UserConverter();
 
     @Test
