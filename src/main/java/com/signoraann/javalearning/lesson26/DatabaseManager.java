90f98891ea4234fabba6fa008dc7c1d85796635d
signoraann <atumanova796@gmail.com>
2026-08-21 21:55:00 +0300

    Added tests and .env.example file

--- src/main/java/com/signoraann/javalearning/lesson26/DatabaseManager.java
+++ src/main/java/com/signoraann/javalearning/lesson26/DatabaseManager.java
@@ -12,7 +12,9 @@ public class DatabaseManager {
         String password = System.getenv("DB_PASSWORD");
 
         if (url == null || user == null || password == null) {
-            throw new IllegalStateException("Database environment variables are missing!");
+            throw new IllegalStateException(
+                    "Database environment variables(DB_URL/DB_USER/DB_PASSWORD) are missing! See .env.example for"
+                            + " details");
         }
         return DriverManager.getConnection(url, user, password);
     }
---
+++ src/test/java/com/signoraann/javalearning/lesson26/DatabaseManagerTest.java
@@ -0,0 +1,18 @@
+package com.signoraann.javalearning.lesson26;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DatabaseManagerTest {
+    @Test
+    void testGetConnectionHappyPath() throws SQLException {
+        try (Connection connection = DatabaseManager.getConnection()) {
+            assertTrue(connection.isValid(2));
+            assertFalse(connection.isClosed());
+        }
+    }
+}
