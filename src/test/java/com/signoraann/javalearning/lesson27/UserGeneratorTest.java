eb6026b0cb83adfeb3531dd586f3a5593f3ceca2
signoraann <atumanova796@gmail.com>
2026-09-16 10:40:48 +0300

    Removed public modifier from UserGeneratorTest class

--- src/test/java/com/signoraann/javalearning/lesson27/UserGeneratorTest.java
+++ src/test/java/com/signoraann/javalearning/lesson27/UserGeneratorTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class UserGeneratorTest {
+class UserGeneratorTest {
     private final UserGenerator userGenerator = new UserGenerator();
 
     @Test
